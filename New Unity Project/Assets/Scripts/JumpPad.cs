fc9f5aab8d42d8c3abbf9ecc1d2a26e4b6527364
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2020-03-09 10:44:09 +0000

    PlatformerGame Complete
    Platformer Game Complete

---
+++ New Unity Project/Assets/Scripts/JumpPad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public Vector2 jumpHeight = new Vector2(0, 5);
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D body = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        if(body != null)
+        {
+            body.AddForce(jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5;
+    public Vector2 jumpHeight = new Vector2(0,5);
+    Rigidbody2D body;
+    Vector2 movement;
+    
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        //vetical is assigned to W and Up Arrow
+        //can be changed in the input manager
+        if(Input.GetButtonDown("Vertical"))
+        {
+            //add an impulse to the rigid body using the jump height
+            body.AddForce(jumpHeight, ForceMode2D.Impulse);
+        }
+
+        //can be multiplied by delta time
+        movement *= movementSpeed;
+
+        //important to copy the Y velocity of the rigid body
+        //ensures that any change due to gravity/jumping are maintained
+        movement.y = body.velocity.y;
+
+        body.velocity = movement;
+    }
+}
